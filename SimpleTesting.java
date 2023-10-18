370184bc832c9e758d8b9fa5c6e08ca134e828e2
Cecsdl <146118084+Cecsdl@users.noreply.github.com>
2023-10-18 11:13:14 +0200

    Update SimpleTesting.java

--- SimpleTesting.java
+++ SimpleTesting.java
@@ -1,8 +1,8 @@
-4
+
 public class SimpleTesting {
 public static void main(String[] args) {
 Student student = new Student("John");
 String name = student.getName();
-System.out.println("Student name is : "+name);
+System.out.println(" my name is : "+name);
 }
 }
